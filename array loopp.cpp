d2be208bc9cc5034eb248a1446b5c821333ddd77
nehathakurOfficial <161203221+nehathakurOfficial@users.noreply.github.com>
2024-02-25 22:05:36 +0530

    Add files via upload

---
+++ array loopp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char vowels[5];
+	for(int idx = 0;idx<5;idx++){
+		cin>>vowels[idx];
+	}
+	for(int idx =0; idx<5; idx++){
+		cout<<vowels[idx]<<" ";
+	}
+	return 0;
+}
